a6a758de3324d1f00ac5035d9d2347f05262703e
Lucas <lsantos7654@gmail.com>
2024-01-11 15:59:59 -0500

    some changes

---
+++ scripts/kde_install.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+# cd ~/Downloads
+# git clone https://github.com/esjeon/krohnkite.git
+# cd krohnkite
+# make install
+mkdir -p ~/.local/share/kservices5/
+ln -s ~/.local/share/kwin/scripts/krohnkite/metadata.desktop ~/.local/share/kservices5/krohnkite.desktop
