bc569ad664342910300de4b9faeeb8e99a914bdb
Lucas <lsantos7654@gmail.com>
2024-01-11 22:05:18 -0500

    final

--- scripts/kde_install.sh
+++ scripts/kde_install.sh
@@ -6,12 +6,15 @@
 # make install
 # mkdir -p ~/.local/share/kservices5/
 # ln -s ~/.local/share/kwin/scripts/krohnkite/metadata.desktop ~/.local/share/kservices5/krohnkite.desktop
-sudo apt install g++ libx11-dev libxext-dev qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev qttools5-dev latte-dock
-sudo add-apt-repository ppa:touchegg/stable
-sudo apt update
-sudo apt install touchegg
-sudo rm -r ~/.config/touchegg
-sudo cp -r ./../.config/touchegg/ ~/.config/
+# sudo apt install g++ libx11-dev libxext-dev qtbase5-dev libqt5svg5-dev libqt5x11extras5-dev libkf5windowsystem-dev qttools5-dev latte-dock
+# curl -sS https://download.spotify.com/debian/pubkey_7A3A762FAFD4A51F.gpg | sudo gpg --dearmor --yes -o /etc/apt/trusted.gpg.d/spotify.gpg
+# echo "deb http://repository.spotify.com stable non-free" | sudo tee /etc/apt/sources.list.d/spotify.list
+# sudo apt-get update && sudo apt-get install spotify-client
+# sudo add-apt-repository ppa:touchegg/stable
+# sudo apt update
+# sudo apt install touchegg
+# sudo rm -r ~/.config/touchegg
+# sudo cp -r ./../.config/touchegg/ ~/.config/
 # sudo add-apt-repository ppa:papirus/papirus
 # sudo apt update
 # sudo apt install qt5-style-kvantum qt5-style-kvantum-themes
@@ -25,3 +28,4 @@ sudo cp -r ./../.config/touchegg/ ~/.config/
 # sudo cp -r ~/Downloads/Dark/plasma/desktoptheme/Ant-Dark/ /usr/share/plasma/desktoptheme/
 # sudo cp -r ~/Downloads/Dark/plasma/look-and-feel/Ant-Dark/ /usr/share/plasma/look-and-feel/
 # sudo cp -r ~/Downloads/Dark/icons/Ant-Dark/ /usr/share/icons/
+# sudo cp -r ~/Downloads/Dark/sddm/Ant-Dark/ /usr/share/sddm/themes/
