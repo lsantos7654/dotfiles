96892a8b055658737f50229cd21f1960a19634ef
Lucas Santos <lsantos@theaiinstitute.com>
2024-07-10 15:22:09 -0400

    update

--- misc/chatgpt.sh
+++ misc/chatgpt.sh
@@ -2,9 +2,8 @@
 # firefox -kiosk -new-window https://chat.openai.com/ &
 
 # Open Firefox with the desired URL
-firefox -new-window https://chat.openai.com/ &
+firefox -new-window https://claude.ai/new &
 
 sleep 1
 
 xdotool key F11
-
