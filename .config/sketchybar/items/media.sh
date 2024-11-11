839e2b6964114708c26738b057e544e72987a324
Lucas Santos <santos@Lucass-MacBook-Pro.local>
2024-11-11 01:16:57 -0500

    update

--- .config/sketchybar/items/media.sh
+++ .config/sketchybar/items/media.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
 sketchybar --add item media e \
-	--set media label.color=$ACCENT_COLOR \
-	label.max_chars=20 \
-	icon.padding_left=0 \
-	scroll_texts=on \
-	icon=􀑪 \
+	--set media \
+	label.color=$ACCENT_COLOR \
 	icon.color=$ACCENT_COLOR \
+	label.max_chars=30 \
+	icon.padding_left=4 \
+	icon.padding_right=4 \
+	label.padding_right=8 \
+	scroll_texts=on \
+	icon=󰝚 \
 	background.drawing=off \
 	script="$PLUGIN_DIR/media.sh" \
-	--subscribe media media_change
+	update_freq=1 \
+	updates=on \
+	associated_space="" \
+	click_script="osascript -e 'tell application \"Spotify\" to playpause'" \
+	--subscribe media media_change mouse.clicked
--- .config/sketchybar/plugins/media.sh
+++ .config/sketchybar/plugins/media.sh
@@ -1,9 +1,46 @@
 #!/bin/bash
 
-STATE="$(echo "$INFO" | jq -r '.state')"
-if [ "$STATE" = "playing" ]; then
-	MEDIA="$(echo "$INFO" | jq -r '.title + " - " + .artist')"
-	sketchybar --set $NAME label="$MEDIA" drawing=on
-else
-	sketchybar --set $NAME drawing=off
-fi
+# Spotify-specific command to get current track info
+get_spotify_info() {
+	osascript -e 'tell application "Spotify"
+        if player state is playing then
+            return "{\"state\": \"playing\", \"artist\": \"" & artist of current track & "\", \"track\": \"" & name of current track & "\"}"
+        else
+            return "{\"state\": \"paused\"}"
+        end if
+    end tell'
+}
+
+# Update function
+update_media() {
+	SPOTIFY_INFO="$(get_spotify_info)"
+	STATE=$(echo "$SPOTIFY_INFO" | jq -r '.state')
+
+	if [ "$STATE" = "playing" ]; then
+		TRACK=$(echo "$SPOTIFY_INFO" | jq -r '.track')
+		ARTIST=$(echo "$SPOTIFY_INFO" | jq -r '.artist')
+
+		# Trim long track/artist names
+		if [ ${#TRACK} -gt 20 ]; then
+			TRACK="$(echo "$TRACK" | cut -c 1-20)..."
+		fi
+		if [ ${#ARTIST} -gt 20 ]; then
+			ARTIST="$(echo "$ARTIST" | cut -c 1-20)..."
+		fi
+
+		MEDIA="$TRACK - $ARTIST"
+		sketchybar --set $NAME label="$MEDIA" drawing=on icon=󰝚
+	else
+		sketchybar --set $NAME drawing=off
+	fi
+}
+
+# Handle incoming events
+case "$SENDER" in
+"media_change")
+	update_media
+	;;
+esac
+
+# Initial update
+update_media
