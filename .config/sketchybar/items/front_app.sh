4657b7de1abcf0a8820821077f2993b7b5b42250
Lucas Santos <santos@Lucass-MacBook-Pro.local>
2024-11-19 22:32:44 -0500

    update

--- .config/sketchybar/items/front_app.sh
+++ .config/sketchybar/items/front_app.sh
@@ -9,7 +9,7 @@ front_app=(
 	script="$PLUGIN_DIR/front_app.sh"
 	icon.drawing=on
 	icon.padding_right=8
-	padding_left=0
+	padding_left=-20
 	label.color=$WHITE
 	icon.color=$WHITE
 	label.font="$FONT:Black:12.0"
@@ -17,6 +17,20 @@ front_app=(
 	associated_display=active
 )
 
+separator=(
+	label="│"
+	label.color=$WHITE
+	label.font="$FONT:Bold:18.0"
+	padding_left=-20
+	padding_right=4
+	associated_display=active
+)
+
+# Add the separator first
+sketchybar --add item separator left \
+	--set separator "${separator[@]}"
+
+# Then add the front_app
 sketchybar --add item front_app left \
 	--set front_app "${front_app[@]}" \
 	--subscribe front_app front_app_switched
